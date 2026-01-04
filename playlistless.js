61743b98999edae2b7c6382a99fe31bbe360540b
EldySparrownest <83872453+EldySparrownest@users.noreply.github.com>
2026-01-04 14:43:14 +0100

    Option to copy pure video link to clipboard

--- playlistless.js
+++ playlistless.js
@@ -12,6 +12,13 @@ browser.contextMenus.create({
   visible: false
 });
 
+browser.contextMenus.create({
+  id: "copy-stripped-link",
+  title: "Copy pure video link (no params)",
+  contexts: ["link"],
+  visible: false
+});
+
 browser.menus.onShown.addListener((info, tab) => {
   //console.log(info);
   const linkUrl = info.linkUrl || "";
@@ -36,13 +43,16 @@ browser.menus.onShown.addListener((info, tab) => {
   browser.menus.update("open-no-playlist-new", {
     visible: isYouTubeList
   });
+  browser.menus.update("copy-stripped-link", {
+    visible: isYouTubeList
+  });
 
   // applying the changes
   browser.menus.refresh();
 });
 
 // strips YouTube playlist parameters
-function stripPlaylistParams(urlString) {
+function stripPlaylistParams(urlString, completely) {
   try {
     let url = new URL(urlString);
     // if url leads to a YouTube video with a playlist parameter
@@ -50,9 +60,17 @@ function stripPlaylistParams(urlString) {
         url.hostname.includes("youtube.com")
         && url.searchParams.has("list")
     ) {
-        url.searchParams.delete("list");
-        if (url.searchParams.has("index")) {
+        if (completely) {
+          for (const key of [...url.searchParams.keys()]) {
+            if (key !== "v") {
+              url.searchParams.delete(key);
+            }
+          }
+        } else {
+          url.searchParams.delete("list");
+          if (url.searchParams.has("index")) {
             url.searchParams.delete("index");
+          }
         }
         return url.toString();
     }
@@ -64,7 +82,7 @@ function stripPlaylistParams(urlString) {
 
 // respond to clicks on context menu items
 browser.contextMenus.onClicked.addListener((info, tab) => {
-  const cleaned = stripPlaylistParams(info.linkUrl);
+  const cleaned = stripPlaylistParams(info.linkUrl, info.menuItemId === "copy-stripped-link");
   if (!cleaned) {
     // not a playlist link; do nothing
     return;
@@ -72,11 +90,20 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
 
   console.log("Playlistless changed url from: " + info.linkUrl + " to " + cleaned);
 
-  if (info.menuItemId === "open-no-playlist-same") {
+  switch (info.menuItemId) {
+    case "open-no-playlist-same":
     // replace current tab with the cleaned URL
     browser.tabs.update(tab.id, { url: cleaned });
-  } else if (info.menuItemId === "open-no-playlist-new") {
+    break;
+    
+    case "open-no-playlist-new":
     // open the cleaned URL in a new tab
     browser.tabs.create({ url: cleaned });
+    break;
+    
+    case "copy-stripped-link":
+    // copy the cleaned URL to clipboard
+    navigator.clipboard.writeText(cleaned);
+    break;
   }
 });
